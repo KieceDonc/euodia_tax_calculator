1fa5639d8d07faa557e66f361f2ec1bcc8a08001
KieceDonc <45487040+KieceDonc@users.noreply.github.com>
2025-07-19 14:14:34 +0200

    ✨ feat: Initialisation avec start.spring.io

---
+++ src/test/java/com/euodia/tax_calculator/TaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euodia.tax_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
